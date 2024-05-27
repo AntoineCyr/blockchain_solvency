e4befda77c73065cf535afb4e7683c910297df60
Antoine Cyr <antoine@nextgenbt.com>
2024-05-27 17:02:03 -0400

    data from blockchain to block

--- src/block.rs
+++ src/block.rs
@@ -1,11 +1,17 @@
 pub type Result<T> = std::result::Result<T, failure::Error>;
+use merkle_sum_tree::{Leaf, MerkleSumTree};
+use std::collections::HashMap;
 
+//work on hash and nonce
 #[derive(Debug, Clone)]
 pub struct Block {
     transactions: Vec<Transaction>,
     prev_block_hash: i32,
     hash: i32,
     nonce: i32,
+    state: HashMap<String, i32>,
+    leaf_index: HashMap<String, usize>,
+    merkle_sum_tree: MerkleSumTree,
 }
 
 #[derive(Debug, Clone)]
@@ -17,18 +23,66 @@ pub struct Transaction {
 
 impl Block {
     pub fn get_hash(&self) -> i32 {
-        self.hash
+        self.hash.clone()
     }
 
-    /// NewBlock creates and returns Block
-    pub fn new_block(transactions: Vec<Transaction>, prev_block_hash: i32) -> Block {
-        let mut block = Block {
-            transactions,
-            prev_block_hash,
-            hash: prev_block_hash + 1,
-            nonce: 0,
-        };
-        block
+    pub fn get_state(&self) -> HashMap<String, i32> {
+        self.state.clone()
+    }
+
+    pub fn get_merkle_sum_tree(&self) -> MerkleSumTree {
+        self.merkle_sum_tree.clone()
+    }
+
+    pub fn new_block(transactions: Vec<Transaction>, prev_block: Option<Block>) -> Result<Block> {
+        match prev_block {
+            None => Ok(Block {
+                transactions,
+                prev_block_hash: 0,
+                hash: 1,
+                nonce: 0,
+                state: HashMap::new(),
+                leaf_index: HashMap::new(),
+                merkle_sum_tree: MerkleSumTree::new(vec![]).unwrap(),
+            }),
+            Some(block) => Self::update_block(block, transactions),
+        }
+    }
+
+    fn update_block(mut block: Block, transactions: Vec<Transaction>) -> Result<Block> {
+        for transaction in transactions {
+            let from: String = transaction.get_from();
+            let to: String = transaction.get_to();
+            let amount: i32 = transaction.get_amount();
+
+            let number_from = match block.state.get(&from) {
+                Some(&number) => number,
+                _ => 0,
+            };
+            let number_to = match block.state.get(&to) {
+                Some(&number) => number,
+                _ => 0,
+            };
+            if from != "" {
+                block.update_state(from, number_from - amount)?;
+            }
+            block.update_state(to, number_to + amount)?;
+        }
+        Ok(block)
+    }
+
+    fn update_state(&mut self, address: String, amount: i32) -> Result<()> {
+        self.state.insert(address.clone(), amount.clone());
+        let index = self.leaf_index.get(&address);
+
+        let leaf = Leaf::new(address.clone(), amount);
+        if index.is_some() {
+            _ = self.merkle_sum_tree.set_leaf(leaf, *index.unwrap());
+        } else {
+            let index = self.merkle_sum_tree.push(leaf).unwrap();
+            self.leaf_index.insert(address, index);
+        }
+        Ok(())
     }
 }
 
--- src/blockchain.rs
+++ src/blockchain.rs
@@ -12,127 +12,67 @@ pub type Result<T> = std::result::Result<T, failure::Error>;
 use std::collections::HashMap;
 
 //handle creating 2 times the same id
+//remove blockchainData
+//Have data for each block? State, merkle tree, ...
 
 //server should print error message
 
 #[derive(Debug, Clone)]
 pub struct Blockchain {
-    blockchain_data: BlockchainData,
-}
-
-#[derive(Debug, Clone)]
-pub struct BlockchainData {
     current_hash: i32,
     mempool: Vec<Transaction>,
     chain: HashMap<i32, Block>,
-    state: HashMap<String, i32>,
-    leaf_index: HashMap<String, usize>,
-    merkle_sum_tree: MerkleSumTree,
 }
 
 impl Blockchain {
-    pub fn get_balance(&mut self, address: String) -> i32 {
-        return match self.blockchain_data.state.get(&address) {
+    pub fn get_balance(&self, address: String) -> i32 {
+        let state = self.chain.get(&self.current_hash).unwrap().get_state();
+        return match state.get(&address) {
             Some(&number) => number,
             _ => -1,
         };
     }
 
-    pub fn get_merkle_sum_tree(&mut self) -> MerkleSumTree {
-        return self.blockchain_data.merkle_sum_tree.clone();
+    pub fn get_merkle_sum_tree(&self) -> MerkleSumTree {
+        self.chain
+            .get(&self.current_hash)
+            .unwrap()
+            .get_merkle_sum_tree()
     }
 
     pub fn create_blockchain() -> Result<Blockchain> {
         let mut chain = HashMap::new();
-        let state = HashMap::new();
-        let leaf_index = HashMap::new();
-        let last_hash = 0;
-        let merkle_sum_tree = MerkleSumTree::new(vec![]).unwrap();
         let mempool = Vec::new();
-        let block = Block::new_block(mempool.clone(), last_hash);
+        let block = Block::new_block(mempool.clone(), None)?;
         let block_hash = block.get_hash();
         chain.insert(block_hash.clone(), block);
-        let bc_data: BlockchainData = BlockchainData {
+        let bc: Blockchain = Blockchain {
             current_hash: block_hash,
             mempool,
             chain,
-            state,
-            leaf_index,
-            merkle_sum_tree,
         };
 
-        Ok(Blockchain {
-            blockchain_data: bc_data,
-        })
+        Ok(bc)
     }
 
     pub fn add_block(&mut self) -> Result<()> {
-        let block = Block::new_block(
-            self.blockchain_data.mempool.clone(),
-            self.blockchain_data.current_hash.clone(),
-        );
-        for transaction in self.blockchain_data.mempool.clone() {
-            let from: String = transaction.get_from();
-            let to: String = transaction.get_to();
-            let amount: i32 = transaction.get_amount();
-
-            let number_from = match self.blockchain_data.state.get(&from) {
-                Some(&number) => number,
-                _ => 0,
-            };
-            let number_to = match self.blockchain_data.state.get(&to) {
-                Some(&number) => number,
-                _ => 0,
-            };
-            if from != "" {
-                self.update_state(from, number_from - amount)?;
-            }
-            self.update_state(to, number_to + amount)?;
-        }
+        let prev_block = self.chain.get(&self.current_hash).unwrap().clone();
+        let block = Block::new_block(self.mempool.clone(), Some(prev_block))?;
 
         println!(
             "new block, number of transactions confirmed: {}",
-            self.blockchain_data.mempool.len()
+            self.mempool.len()
         );
-        self.blockchain_data.mempool.clear();
-        self.blockchain_data
-            .chain
-            .insert(block.get_hash(), block.clone());
-        self.blockchain_data.current_hash = block.get_hash();
+        self.mempool.clear();
+        self.current_hash = block.get_hash();
+        self.chain.insert(block.get_hash(), block);
 
         Ok(())
     }
 
     pub fn add_transaction(&mut self, from: String, to: String, amount: i32) -> Result<()> {
         let transaction = Transaction::new(from, to, amount);
-        self.blockchain_data.mempool.push(transaction);
-
-        Ok(())
-    }
-
-    fn update_state(&mut self, address: String, amount: i32) -> Result<()> {
-        self.blockchain_data
-            .state
-            .insert(address.clone(), amount.clone());
-        let index = self.blockchain_data.leaf_index.get(&address);
-        println!("leaf_index: {:?}", {
-            self.blockchain_data.leaf_index.clone()
-        });
-        println!("index: {:?}", index);
-        //println!("index unwrap: {:?}", *index.unwrap());
-
-        let leaf = Leaf::new(address.clone(), amount);
-        if index.is_some() {
-            _ = self
-                .blockchain_data
-                .merkle_sum_tree
-                .set_leaf(leaf, *index.unwrap());
-        } else {
-            let index = self.blockchain_data.merkle_sum_tree.push(leaf).unwrap();
-            println!("new index: {:?}", index);
-            self.blockchain_data.leaf_index.insert(address, index);
-            println!("{:?}", self.blockchain_data.merkle_sum_tree.get_leafs());
-        }
+        self.mempool.push(transaction);
 
         Ok(())
     }
